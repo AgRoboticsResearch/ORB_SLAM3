c9483bc7ac25c214462ca852083e563453e302f8
ZFEI <zfei@zju.edu.cn>
2024-07-29 16:59:15 +0800

    update

--- build_ros.sh
+++ build_ros.sh
@@ -1,5 +1,5 @@
 echo "Building ROS nodes"
-export ROS_PACKAGE_PATH=${ROS_PACKAGE_PATH}:/codes/ORB_SLAM3_docker/Examples/ROS
+export ROS_PACKAGE_PATH=${ROS_PACKAGE_PATH}:/codes/ORB_SLAM3/Examples/ROS
 export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
 cd Examples/ROS/ORB_SLAM3
 rm -r build
